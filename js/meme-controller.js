ee5009394bb1e1d07b5c5479332986ec90de4c51
Guy Dahan <guy4191@gmail.com>
2023-06-21 20:07:54 +0300

    first commit - text on canvas

---
+++ js/meme-controller.js
@@ -0,0 +1,68 @@
+'use strict'
+
+let gElCanvas = {}
+let gCtx
+
+////////////////////////////////////  init  //////////////////////////////////
+
+function onInitMeme() {
+    resizeCanvas()
+    setCanvas()
+    renderMeme()
+    // addListeners()
+}
+
+////////////////////////////////  render  ///////////////////////////////////
+
+function renderMeme() {
+    const meme = getMeme()
+    renderImg(meme.selectedImgId)
+    setTimeout(() => {
+        renderLines(meme)
+    }, 100);
+
+}
+
+function renderImg(idx) {
+    drawImg(idx)
+}
+
+function renderLines(meme) {
+    const lines = meme.lines
+    lines.forEach((line) => {
+        drawText(line)
+    })
+}
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    console.log('elContainer.offsetWidth:', elContainer.offsetWidth)
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+}
+
+////////////////////////////////  ev-colors  ////////////////////////////////
+
+// function onPickFillColor(color) {
+//     gCurrColors.fill = color
+// }
+
+// function onPickStrokeColor(color) {
+//     gCurrColors.stroke = color
+// }
+
+// function getColor() {
+//     return gCurrColors
+// }
+
+////////////////////////////////  canvas  ////////////////////////////////
+
+function setCanvas() {
+    gElCanvas = document.querySelector('#my-canvas')
+    gCtx = gElCanvas.getContext('2d')
+}
+
+function onSetLineTxt(txt) {
+    setLineTxt(txt)
+    renderMeme()
+}
